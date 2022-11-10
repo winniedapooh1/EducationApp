5797ff34b5f4c7aafac1f0430273df2797d379c1
29jva <28jvarkey@gmail.com>
2022-11-09 23:14:10 -0500

    Made a new class that stored all the methods so its better for readablity

--- src/EducationApp.java
+++ src/EducationApp.java
@@ -30,17 +30,23 @@ public class EducationApp extends Student {
         int index = 0;
         MyTimer mT = new MyTimer();
         mT.set(10000);
+        test test = new test();
         /*
-         * You still have to make a loop that loops through to see if they hava an account
-         * make sure to make a check to see if the user exist in the student array list
+         ? You need to make a class that hold all of the methods that you use to make readablity better
+         *
+         *
+         *
+         *
+         ! You still have to make a loop that loops through to see if they hava an account
+         ! make sure to make a check to see if the user exist in the student array list
          */
-        insertStudent(students, fileNameStudents);
-        //System.out.println(students.get(0).getName());
+        test.insertStudent(students, fileNameStudents);
+        System.out.println(students.get(0).getName());
         System.out.println("Are you a new user?(yes or no)");
         yesNo = uI.nextLine();
         while (yesNo.equals("yes")) {
             /*
-             * make a method that checks is the username or password is already taken
+             ! make a method that checks is the username or password is already taken
              */
 
             System.out.println("Please enter your new user name: ");
@@ -106,14 +112,16 @@ public class EducationApp extends Student {
             int answerCount = 0;
             int wrongAnswer = 0;
 
-            menu();
+            test.menu();
             yesNo = uI.nextLine();
             if (yesNo.equals("1")) {
-                insertMathQuestions(addQuestions, addAnswer, fileNameAdd);
+                test.insertMathQuestions(addQuestions, addAnswer, fileNameAdd);
                 /*
-                 * We should add a feature where it randomly give the user a index form the add array(DONE)
+                 ? We should add a feature where it randomly give the user a index form the add array(DONE)
+                 *
                  * The timer works and such
                  * How the random works:
+                 *
                  * I stored the max size of the array in an int varable called max
                  * then you just multiply it by Math.random()
                  */
@@ -127,8 +135,8 @@ public class EducationApp extends Student {
                      * the If statement does a check to see if there is time left
                      *
                      *
-                     * (need to add in the wrong and correct answer count into the
-                     * the student class)
+                     ! (need to add in the wrong and correct answer count into the
+                     ! the student class)
                      */
                     int randNum = (int)(Math.random() * max);
                     System.out.println(addQuestions.get(randNum));
@@ -146,14 +154,14 @@ public class EducationApp extends Student {
             /*
              * Pleas eneter the the code for the program here
              *
-             * Create a menu  for the user to pick what they want to do
-             * need 4> if statments that have loops through
+             ? Create a menu  for the user to pick what they want to do
+             ! need 4> if statments that have loops through
              *
              *
-             * create  method that checks of a answer is correct or not
-             * create variable that hold the ammount of correct answers
-             *    * you might need to cereate a new private varble in Student
-             *    * maybe one for each txt file or something
+             ? create  method that checks of a answer is correct or not
+             ? create variable that hold the ammount of correct answers
+             !    * you might need to cereate a new private varble in Student
+             !    * maybe one for each txt file or something
              *    *
              *
              *
@@ -161,142 +169,5 @@ public class EducationApp extends Student {
              */
 
         }
-    }
-
-    public static void insertMathQuestions(ArrayList<String> mathEx, ArrayList<String> Answer, String filename) {
-        try {
-            /*
-             * Think of scanner as blank notepads
-             */
-            FileReader fr = new FileReader(filename);
-            Scanner sC = new Scanner(fr);
-            /*
-             * Makes blank notepad of txt file
-             */
-            while (sC.hasNext()) {
-                /*
-                 *Instead of using buffred reader it is easier to use scanners
-                 * to insert into an ArrayList
-                 */
-                String txt = sC.nextLine();
-                /*
-                 * txt catches the entire first line include the ";"
-                 */
-                Scanner sCT = new Scanner(txt);
-                /*
-                 * sCT is a new blank notepad the holds txt String
-                 */
-                sCT.useDelimiter(";"); // This is the same as .split()
-                String mathExpression = "";
-                String answer = "";
-                //String rate = "";
-                mathExpression = sCT.next();
-                mathEx.add(mathExpression);
-                answer = sCT.next();
-                Answer.add(answer);
-            }
-            sC.close();
-        } catch (FileNotFoundException badFile) {
-            System.out.println("Trouble finding file");
-        } catch (ArrayIndexOutOfBoundsException toofar) {
-            System.out.println("index out of bonunds");
-        }
-    }
-
-    public static void insertStudent(ArrayList<Student> students, String filename) {
-        try {
-            /*
-             * Think of scanner of blank notepads
-             */
-            FileReader fr = new FileReader(filename);
-            Scanner sC = new Scanner(fr);
-            /*
-             * Makes blank notepad of txt file
-             */
-            while (sC.hasNext()) {
-                /*
-                 *Instead of using buffred reader it is easier to use scanners
-                 * to insert into an ArrayList
-                 */
-                String txt = sC.nextLine();
-                /*
-                 * txt catches the entire first line include the ";"
-                 */
-                Scanner sCT = new Scanner(txt);
-                /*
-                 * sCT is a new blank notepad the holds txt String
-                 */
-                sCT.useDelimiter(";"); // This is the same as .split()
-                String username = "";
-                String password = "";
-                String level = "";
-                String name = "";
-                username = sCT.next();
-                password = sCT.next();
-                level = sCT.next();
-                name = sCT.next();
-                Student obj = new Student(username, password, level, name);
-                students.add(obj);
-                /*
-                 * This basically finds the next string in the line and insersts it into a String and makes
-                 * a obj and then adds that into a Arraylist
-                 */
-            }
-            sC.close();
-        } catch (FileNotFoundException badFile) {
-            System.out.println("Trouble finding file");
-        } catch (ArrayIndexOutOfBoundsException toofar) {
-            System.out.println("index out of bonunds");
-        }
-    }
-
-    public static void menu() {
-        System.out.println("Please enter the number the number of the tyoe of math problem you want to practice ");
-        System.out.println("1. Addition");
-        System.out.println("2. subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
-
-    }
-
-    public static boolean taken(String username, String password, ArrayList<Student> Student) {
-        /*
-         * This method will check to see if the new username is taken
-         */
-        for (int i = 0; i < Student.size(); i++) {
-            if (username.equals(Student.get(i))) {
-                System.out.println("Please enter a diffrent username");
-            }
-        }
-        return false;
-    }
-
-    public static void write(String fileName) {
-        String math = "";
-        int answer = 0;
-        /*
-         * This could work but it could also give us a index out of bounds erro
-         * I didnt complete it yet
-         * Made on 11/6/22
-         * This method is writing to a text file so I dont have too
-         *
-         */
-        try {
-            FileWriter myWriter = new FileWriter(fileName, true);
-            BufferedWriter bW = new BufferedWriter(myWriter);
-            for (int i = 1; i < 11; i++) {
-                answer = i + i + 1;
-                math = Integer.toString(i) + "+" + Integer.toString(i + 1) + ";" + Integer.toString(answer);
-                myWriter.write(math + System.getProperty("line.separator"));
-            }
-            myWriter.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            /*
-             * printStackTrace pinpoints the error in the code
-             */
-            e.printStackTrace();
-        }
-
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,10 +1,159 @@
+import java.io.*;
 import java.util.*;
 public class test {
+    /*
+     * This class is used for testing code and storing methods so the readability of the runner class is better
+     */
     public static void main(String []args){
 
          for(int i = 0; i < 10; i ++){
              int ran = (int)(Math.random() * 10);
              System.out.println(ran);
          }
+    }
+    public static void write(String fileName) {
+        String math = "";
+        int answer = 0;
+        /*
+         * This could work but it could also give us a index out of bounds erro
+         * I didnt complete it yet
+         * Made on 11/6/22
+         * This method is writing to a text file so I dont have too
+         *
+         */
+        try {
+            FileWriter myWriter = new FileWriter(fileName, true);
+            BufferedWriter bW = new BufferedWriter(myWriter);
+            for (int i = 1; i < 11; i++) {
+                answer = i + i + 1;
+                math = Integer.toString(i) + "+" + Integer.toString(i + 1) + ";" + Integer.toString(answer);
+                myWriter.write(math + System.getProperty("line.separator"));
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            /*
+             * printStackTrace pinpoints the error in the code
+             */
+            e.printStackTrace();
+        }
+
+    }
+    public static boolean takenUserName(String username, ArrayList<Student> Student) {
+        /*
+         * This method will check to see if the new username is taken
+         */
+        for (int i = 0; i < Student.size(); i++) {
+            if (username.equals(Student.get(i))) {
+                System.out.println("Please enter a diffrent username");
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean takenPassword(String password, ArrayList<Student> Student) {
+        /*
+         * This method will check to see if the new username is taken
+         */
+        for (int i = 0; i < Student.size(); i++) {
+            if (password.equals(Student.get(i))) {
+                System.out.println("Please enter a diffrent username");
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void insertStudent(ArrayList<Student> students, String filename) {
+        try {
+            /*
+             * Think of scanner of blank notepads
+             */
+            FileReader fr = new FileReader(filename);
+            Scanner sC = new Scanner(fr);
+            /*
+             * Makes blank notepad of txt file
+             */
+            while (sC.hasNext()) {
+                /*
+                 *Instead of using buffred reader it is easier to use scanners
+                 * to insert into an ArrayList
+                 */
+                String txt = sC.nextLine();
+                /*
+                 * txt catches the entire first line include the ";"
+                 */
+                Scanner sCT = new Scanner(txt);
+                /*
+                 * sCT is a new blank notepad the holds txt String
+                 */
+                sCT.useDelimiter(";"); // This is the same as .split()
+                String username = "";
+                String password = "";
+                String level = "";
+                String name = "";
+                username = sCT.next();
+                password = sCT.next();
+                level = sCT.next();
+                name = sCT.next();
+                Student obj = new Student(username, password, level, name);
+                students.add(obj);
+                /*
+                 * This basically finds the next string in the line and insersts it into a String and makes
+                 * a obj and then adds that into a Arraylist
+                 */
+            }
+            sC.close();
+        } catch (FileNotFoundException badFile) {
+            System.out.println("Trouble finding file");
+        } catch (ArrayIndexOutOfBoundsException toofar) {
+            System.out.println("index out of bonunds");
+        }
+    }
+    public static void menu() {
+        System.out.println("Please enter the number the number of the tyoe of math problem you want to practice ");
+        System.out.println("1. Addition");
+        System.out.println("2. subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+    }
+    public static void insertMathQuestions(ArrayList<String> mathEx, ArrayList<String> Answer, String filename) {
+        try {
+            /*
+             * Think of scanner as blank notepads
+             */
+            FileReader fr = new FileReader(filename);
+            Scanner sC = new Scanner(fr);
+            /*
+             * Makes blank notepad of txt file
+             */
+            while (sC.hasNext()) {
+                /*
+                 *Instead of using buffred reader it is easier to use scanners
+                 * to insert into an ArrayList
+                 */
+                String txt = sC.nextLine();
+                /*
+                 * txt catches the entire first line include the ";"
+                 */
+                Scanner sCT = new Scanner(txt);
+                /*
+                 * sCT is a new blank notepad the holds txt String
+                 */
+                sCT.useDelimiter(";"); // This is the same as .split()
+                String mathExpression = "";
+                String answer = "";
+                //String rate = "";
+                mathExpression = sCT.next();
+                mathEx.add(mathExpression);
+                answer = sCT.next();
+                Answer.add(answer);
+            }
+            sC.close();
+        } catch (FileNotFoundException badFile) {
+            System.out.println("Trouble finding file");
+        } catch (ArrayIndexOutOfBoundsException toofar) {
+            System.out.println("index out of bonunds");
+        }
     }
 }
